f77c11c618f857f82308ef55d92ea69112b3a577
Tri Nguyen <snico90@gmail.com>
2019-11-11 11:07:16 -0500

    update app.js file and pie chart

--- Belly_Button_Diversity/Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Diversity/Belly_Button_Biodiversity/static/js/app.js
@@ -1,107 +1,79 @@
 function buildMetadata(sample) {
-
-  // @TODO: Complete the following function that builds the metadata panel
-
-  // Use `d3.json` to fetch the metadata for a sample
+  d3.json(`/metadata/${sample}`).then((data) => {
     // Use d3 to select the panel with id of `#sample-metadata`
-    let sample_endpoint = '/metadata/' + sample;
-    d3.json(sample_endpoint)
-      .then(function(response){
-        // response: {"AGE":68.0,"BBTYPE":"I","ETHNICITY":"Caucasian","GENDER":"F","LOCATION":null,"WFREQ":1.0,"sample":1293}
+    var PANEL = d3.select("#sample-metadata");
 
-        // console.log('response:', response);
-        // We do stuff with the data the comes back from the API
-        // Target the METADATA element so that we can populate it with the gathered info
-        let metaDataElement = d3.select('#sample-metadata');
+    // Use `.html("") to clear any existing metadata
+    PANEL.html("");
 
-        // Use `.html("") to clear any existing metadata
-        metaDataElement.html("");
-    
-        // Use `Object.entries` to add each key and value pair to the panel
-        // Hint: Inside the loop, you will need to use d3 to append new
-        // tags for each key-value in the metadata.
-        // console.log('Object entries:', Object.entries(response));
-        Object.entries(response).forEach(function([key, value]) {
-          let text = key + ': ' + value;
-          metaDataElement.append('p').text(text);
-        })
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.entries(data).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key}: ${value}`);
+    });
 
-        // BONUS: Build the Gauge Chart, uncomment when ready to test
-        // buildGauge(response.WFREQ);
-      })
-
+    // BONUS: Build the Gauge Chart
+    buildGauge(data.WFREQ);
+  });
 }
 
 function buildCharts(sample) {
+  d3.json(`/samples/${sample}`).then((data) => {
+    const otu_ids = data.otu_ids;
+    const otu_labels = data.otu_labels;
+    const sample_values = data.sample_values;
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
-    var sample_endpoint = '/samples/' + sample;
-    d3.json(sample_endpoint)
-      .then(function(response) {
-        // @TODO: Build a Bubble Chart using the sample data
-        console.log('[buildCharts] response:', response);
-
-        // Grab our 3 lists of information separately
-        var labels = response['otu_labels'];
-        var values = response['otu_ids'];
-        var size = response['sample_values'];
-
-        // Create a 'payload' to feed to the chart function
-        var bubbleChartData = [
-          {
-            'x': values,
-            'y': size,
-            'text': labels,
-            'mode': 'markers',
-            'marker': {
-              size: size,
-              color: values
-            }
-          }
-        ];
-
-        // Create a layout for the bubble chart
-        var bubbleChartLayout = {
-          'xaxis': { 'title': 'OTU IDS'},
-
+    // Build a Bubble Chart
+    var bubbleLayout = {
+      margin: { t: 0 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" }
+    };
+    var bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
         }
+      }
+    ];
 
-        // build the bubble chart
-        Plotly.plot('bubble', bubbleChartData, bubbleChartLayout);
+    Plotly.plot("bubble", bubbleData, bubbleLayout);
 
-        // @TODO: Build a Pie Chart
-        // HINT: You will need to use slice() to grab the top 10 sample_values,
-        // otu_ids, and labels (10 each).
+    // Build a Pie Chart
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+    var pieData = [
+      {
+        values: sample_values.slice(0, 10),
+        labels: otu_ids.slice(0, 10),
+        hovertext: otu_labels.slice(0, 10),
+        hoverinfo: "hovertext",
+        type: "pie"
+      }
+    ];
 
-      // Building Pie Chart
-        var pieData = [{
-          values: sample_values.slice(0,10),
-          labels: otu_ids.slice(0,10),
-          hovertext: otu_labels.slice(0,10,),
-          hoverinfo: 'hovertext',
-          type: 'pie'
-        }];
+    var pieLayout = {
+      margin: { t: 0, l: 0 }
+    };
 
-        var pieLayout = {
-          margin: {t: 0, l: 0}
-        }
-
-        Plotly.plot('pie', pieData, pieLayout); 
-
-    });
-  }
+    Plotly.plot("pie", pieData, pieLayout);
+  });
+}
 
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("/names").then(function(sampleNames) {
-    // sampleNames = ["940","941","943","944","945","946", ...]
-
-    // Build the dropdown menu items (options)
-    sampleNames.forEach(function(sample) {
-      // sample = "940"
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
       selector
         .append("option")
         .text(sample)
@@ -120,9 +92,6 @@ function optionChanged(newSample) {
   buildCharts(newSample);
   buildMetadata(newSample);
 }
-
-// def make_pizza():
-//   print('pizza is made')
 
 // Initialize the dashboard
 init();
